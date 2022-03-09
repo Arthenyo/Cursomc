0493ab56647bde2e26c0f6273f5833bbba982300
Arthenyo <arthenyocarlos19@gmail.com>
2022-03-09 09:21:55 -0300

    PROJETO CRIADO

---
+++ src/test/java/com/arthenyocarlos/aulasmc/AulasmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthenyocarlos.aulasmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
